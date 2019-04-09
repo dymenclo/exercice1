4b96f073fc9547e375d401f2e6c52e91914fa376
dymenclo <49412872+dymenclo@users.noreply.github.com>
2019-04-09 09:17:19 +0000

    Add files via upload

---
+++ moyenne.py
@@ -0,0 +1,15 @@
+
+
+print "Bienvenu chez Natalie : " 
+ 
+x = input("Enter la premiere valeur :")
+y = input("Enter la deuxieme valeur :")
+
+somme = x + y
+moyenne = somme / 2
+ 
+print ("la somme de ces elements est : " somme)
+print ("la moyenne de ces elements est : "moyenne)
+ 
+
+
